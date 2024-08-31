e807af5376c09c3b54276be02c678313e6960016
0NIHILITY0 <johnbuenaflor2108@gmail.com>
2024-08-31 16:51:08 +0800

    init

--- Lab-Earth_1/Output/sketch/Lab-Earth_1.ino.cpp
+++ Lab-Earth_1/Output/sketch/Lab-Earth_1.ino.cpp
@@ -2,6 +2,11 @@
 #line 1 "C:\\Users\\John Buenaflor\\Git_Repo\\LEC-Xinyx\\Lab-Earth_1\\Lab-Earth_1.ino"
 
 
+#line 3 "C:\\Users\\John Buenaflor\\Git_Repo\\LEC-Xinyx\\Lab-Earth_1\\Lab-Earth_1.ino"
+void setup();
+#line 6 "C:\\Users\\John Buenaflor\\Git_Repo\\LEC-Xinyx\\Lab-Earth_1\\Lab-Earth_1.ino"
+void loop();
+#line 3 "C:\\Users\\John Buenaflor\\Git_Repo\\LEC-Xinyx\\Lab-Earth_1\\Lab-Earth_1.ino"
 void setup(){
   Serial.begin (115200);
 }
